3b249ed20ff523a6802e3b80975fc247f2a2b6c3
kimhyunmook <gusanr4200@naver.com>
2024-01-22 23:33:13 +0900

    calendar work  01/22

--- client/src/store/sagas/calendarSaga.jsx
+++ client/src/store/sagas/calendarSaga.jsx
@@ -1,11 +1,20 @@
 import { call, put, all, delay } from "redux-saga/effects";
 import { commonAPi } from "../api/api";
-import { calendarInfo, getCalendarData } from "../calendarSlice";
+import { getCalendarData, getNotTodayData } from "../calendarSlice";
 
 export function* handleCalendarInfo(body) {
   try {
     const res = yield call(commonAPi.post, body);
     yield put(getCalendarData(res));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export function* handle_notToday(body) {
+  try {
+    const res = yield call(commonAPi.post, body)
+    yield put(getNotTodayData(res))
   } catch (error) {
     console.error(error);
   }
