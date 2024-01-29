101684f2a1f20038ea220400049efeead4f22f72
gusanr4200@naver.com <gusanr4200@naver.com>
2024-01-29 17:29:00 +0900

    add aside and main fixed

--- client/src/component/common/footer.jsx
+++ client/src/component/common/footer.jsx
@@ -1,11 +1,13 @@
-import Logo from "./logo"
+import AdminSide from "./adminSide";
+import Logo from "./logo";
 
 export default function Footer(props) {
-    return (
-        <footer>
-            <div className="max_container">
-                <Logo></Logo>
-            </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <AdminSide></AdminSide>
+      <div className="max_container">
+        <Logo></Logo>
+      </div>
+    </footer>
+  );
 }
